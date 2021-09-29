324c1fd852bc296007a3e63ee996b4e92d195931
rupak <rupak.rb2001@gmail.com>
2021-09-29 12:10:17 +0530

    added images to readme

--- README.md
+++ README.md
@@ -2,16 +2,30 @@
 
 The docs page ui is created with React js along With Tailblocks and Tailwind customizations
 
-##Demo deployment link
+### Demo deployment link
 [name](limk)
 
 
 ## FEATURES:
 
-### `1)Dark Mode/Light Mode`
-### `2)Completely responsive`
-### `3)Self Made document storing logic(game of css)(follow comments inside jsx files to customize)`
-### `4)Color combinations easier for eyes and gives modern touch`
+### 1)Dark Mode/Light Mode
+### 2)Completely responsive
+### 3)Self Made document storing logic(game of css)(follow comments inside jsx files to customize)
+### 4)Color combinations easier for eyes and gives modern touch
+### 5)Can add code snippets and also do copy in one click(follow comments to add more)
+
+
+for now only 3 dummy links i have made for demonstration(for first 2 big tiles and first 1 small tile. and in the doc page 3 first 3 links will work (others will work if you put contents and link them))
+
+## A few Demo images are given below
+
+
+![1st image](https://github.com/rupak2001/SPERAX_NAGV_UI/blob/main/readme_images/1.png?raw=true)
+![1st image](https://github.com/rupak2001/SPERAX_NAGV_UI/blob/main/readme_images/2.png?raw=true)
+![1st image](https://github.com/rupak2001/SPERAX_NAGV_UI/blob/main/readme_images/3.png?raw=true)
+![1st image](https://github.com/rupak2001/SPERAX_NAGV_UI/blob/main/readme_images/4.png?raw=true)
+![1st image](https://github.com/rupak2001/SPERAX_NAGV_UI/blob/main/readme_images/5.png?raw=true)
+![1st image](https://github.com/rupak2001/SPERAX_NAGV_UI/blob/main/readme_images/6.png?raw=true)
 
 
 
--- src/docXplorer.jsx
+++ src/docXplorer.jsx
@@ -164,14 +164,14 @@ var Docxplorer = (props) => {
         <div className="docpagediv" style={{ backgroundColor: change_bg_ini.change_bg_color, height: "100%", width: "100%" }}>
             <header className={change_bg_ini.change_boxcolor} style={{ position: "fixed", zIndex: 10, width: "100%" }}>
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a href = "https://sperax.io/"><img id="speraxlogo" src={change_bg_ini.change_logo} style={{ cursor: "pointer" }} /></a>
+                    <a href="https://sperax.io/"><img id="speraxlogo" src={change_bg_ini.change_logo} style={{ cursor: "pointer" }} /></a>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                        <a href = "https://sperax.io/USDs" className="mr-5 hover:text-white">USD'S</a>
-                        <a href = "https://sperax.io/team" className="mr-5 hover:text-white">About Us</a>
-                        <a href = "https://sperax.io/#Resources" className="mr-5 hover:text-white">Resources</a>
-                        <a href = "https://sperax.io/put your endpoint here" className="mr-5 hover:text-white">Docs</a>
-                        <a href = "https://sperax.io/news" className="mr-5 hover:text-white">News</a>
-                        <a href = "https://forum.sperax.io/" className="mr-5 hover:text-white">Forum</a>
+                        <a href="https://sperax.io/USDs" className="mr-5 hover:text-white">USD'S</a>
+                        <a href="https://sperax.io/team" className="mr-5 hover:text-white">About Us</a>
+                        <a href="https://sperax.io/#Resources" className="mr-5 hover:text-white">Resources</a>
+                        <a href="https://sperax.io/put your endpoint here" className="mr-5 hover:text-white">Docs</a>
+                        <a href="https://sperax.io/news" className="mr-5 hover:text-white">News</a>
+                        <a href="https://forum.sperax.io/" className="mr-5 hover:text-white">Forum</a>
                     </nav>
                     <Switch color="warning" onClick={() => { change_bg() }} />
                     {change_bg_ini.change_icon}
@@ -493,7 +493,7 @@ var Docxplorer = (props) => {
             <footer class={change_bg_ini.change_boxcolor}>
                 <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
                     <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-                        <a href = "https://sperax.io/" class="flex title-font font-medium items-center md:justify-start justify-center text-white">
+                        <a href="https://sperax.io/" class="flex title-font font-medium items-center md:justify-start justify-center text-white">
                             <img id="speraxlogo" src={change_bg_ini.change_logo} />
                         </a>
                         <p class={change_bg_ini.change_txtcolor}>Connecting you with modern money</p>
@@ -503,10 +503,10 @@ var Docxplorer = (props) => {
                             <h2 class={change_bg_ini.change_headertxt}>Projects</h2>
                             <nav class="list-none mb-10">
                                 <li>
-                                    <a href = "https://sperax.io/bdls" class={change_bg_ini.change_txtcolor}>BDLS</a>
+                                    <a href="https://sperax.io/bdls" class={change_bg_ini.change_txtcolor}>BDLS</a>
                                 </li>
                                 <li>
-                                    <a href = "https://sperax.io/lifted" class={change_bg_ini.change_txtcolor}>LIFTED</a>
+                                    <a href="https://sperax.io/lifted" class={change_bg_ini.change_txtcolor}>LIFTED</a>
                                 </li>
                             </nav>
                         </div>
@@ -514,24 +514,24 @@ var Docxplorer = (props) => {
                             <h2 class={change_bg_ini.change_headertxt}>About</h2>
                             <nav class="list-none mb-10">
                                 <li>
-                                    <a href = "https://sperax.io/team" class={change_bg_ini.change_txtcolor}>Team</a>
+                                    <a href="https://sperax.io/team" class={change_bg_ini.change_txtcolor}>Team</a>
                                 </li>
                                 <li>
-                                    <a href = "https://sperax.io/story" class={change_bg_ini.change_txtcolor}>The Sperax Story</a>
+                                    <a href="https://sperax.io/story" class={change_bg_ini.change_txtcolor}>The Sperax Story</a>
                                 </li>
                                 <li>
-                                    <a href = "https://sperax.io/#Timeline" class={change_bg_ini.change_txtcolor}>Roadmap</a>
+                                    <a href="https://sperax.io/#Timeline" class={change_bg_ini.change_txtcolor}>Roadmap</a>
                                 </li>
                                 <li>
-                                    <a href = "https://sperax.io/news" class={change_bg_ini.change_txtcolor}>News</a>
+                                    <a href="https://sperax.io/news" class={change_bg_ini.change_txtcolor}>News</a>
                                 </li>
                             </nav>
                         </div>
                         <div class="lg:w-1/3 md:w-1/2 w-full px-4">
-                            <a href = "https://sperax.io/developers" class={change_bg_ini.change_headertxt}>Developers</a>
+                            <a href="https://sperax.io/developers" class={change_bg_ini.change_headertxt}>Developers</a>
                             <nav class="list-none mb-10">
                                 <li>
-                                    <a href = "https://gitcoin.co/hackathon/gr11?org=sperax" class={change_bg_ini.change_txtcolor}>Bounties</a>
+                                    <a href="https://gitcoin.co/hackathon/gr11?org=sperax" class={change_bg_ini.change_txtcolor}>Bounties</a>
                                 </li>
                             </nav>
                         </div>
@@ -539,13 +539,13 @@ var Docxplorer = (props) => {
                             <h2 class={change_bg_ini.change_headertxt}>Resources</h2>
                             <nav class="list-none mb-10">
                                 <li>
-                                    <a href = "https://github.com/Sperax/USDs-Whitepaper/blob/main/USDs_Whitepaper.pdf" class={change_bg_ini.change_txtcolor}>Whitepaper</a>
+                                    <a href="https://github.com/Sperax/USDs-Whitepaper/blob/main/USDs_Whitepaper.pdf" class={change_bg_ini.change_txtcolor}>Whitepaper</a>
                                 </li>
                                 <li>
-                                    <a href = "https://sperax.io/brandKit" class={change_bg_ini.change_txtcolor}>BrandKit</a>
+                                    <a href="https://sperax.io/brandKit" class={change_bg_ini.change_txtcolor}>BrandKit</a>
                                 </li>
                                 <li>
-                                    <a href = "https://etherscan.io/token/0xb4a3b0faf0ab53df58001804dda5bfc6a3d59008" class={change_bg_ini.change_txtcolor}>erc20 Block Explorer</a>
+                                    <a href="https://etherscan.io/token/0xb4a3b0faf0ab53df58001804dda5bfc6a3d59008" class={change_bg_ini.change_txtcolor}>erc20 Block Explorer</a>
                                 </li>
                             </nav>
                         </div>
@@ -553,18 +553,18 @@ var Docxplorer = (props) => {
                             <h2 class={change_bg_ini.change_headertxt}>Contact</h2>
                             <nav class="list-none mb-10">
                                 <li>
-                                    <a href = "contact@sperax.io" class={change_bg_ini.change_txtcolor}>Email</a>
+                                    <a href="contact@sperax.io" class={change_bg_ini.change_txtcolor}>Email</a>
                                 </li>
                                 <li>
-                                    <a href = "https://sperax.io/careers" class={change_bg_ini.change_txtcolor}>Career</a>
+                                    <a href="https://sperax.io/careers" class={change_bg_ini.change_txtcolor}>Career</a>
                                 </li>
                             </nav>
                         </div>
                         <div class="lg:w-1/3 md:w-1/2 w-full px-4">
-                            <a href = "https://forum.sperax.io/" class={change_bg_ini.change_headertxt}>Forum</a>
+                            <a href="https://forum.sperax.io/" class={change_bg_ini.change_headertxt}>Forum</a>
                             <nav class="list-none mb-10">
                                 <li>
-                                    <a href = "https://sperax.io/play" class={change_bg_ini.change_headertxt}>Sperax Play</a>
+                                    <a href="https://sperax.io/play" class={change_bg_ini.change_headertxt}>Sperax Play</a>
                                 </li>
                             </nav>
                         </div>
@@ -575,22 +575,22 @@ var Docxplorer = (props) => {
                         <p class="text-gray-400 text-sm text-center sm:text-left">Sperax Foundation © Sperax 2021. All rights reserved.
                         </p>
                         <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                            <a href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg4NTI1MzU3MA==&scene=124#wechat_redirect" class="text-gray-400">
+                            <a href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg4NTI1MzU3MA==&scene=124#wechat_redirect" class="text-gray-400">
                                 <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                <path d="M21.502 19.525c1.524-1.105 2.498-2.738 2.498-4.554 0-3.326-3.237-6.023-7.229-6.023s-7.229 2.697-7.229 6.023c0 3.327 3.237 6.024 7.229 6.024.825 0 1.621-.117 2.36-.33l.212-.032c.139 0 .265.043.384.111l1.583.914.139.045c.133 0 .241-.108.241-.241l-.039-.176-.326-1.215-.025-.154c0-.162.08-.305.202-.392zm-12.827-17.228c-4.791 0-8.675 3.236-8.675 7.229 0 2.178 1.168 4.139 2.997 5.464.147.104.243.276.243.471l-.03.184-.391 1.458-.047.211c0 .16.13.29.289.29l.168-.054 1.899-1.097c.142-.082.293-.133.46-.133l.255.038c.886.255 1.842.397 2.832.397l.476-.012c-.188-.564-.291-1.158-.291-1.771 0-3.641 3.542-6.593 7.911-6.593l.471.012c-.653-3.453-4.24-6.094-8.567-6.094zm5.686 11.711c-.532 0-.963-.432-.963-.964 0-.533.431-.964.963-.964.533 0 .964.431.964.964 0 .532-.431.964-.964.964zm4.82 0c-.533 0-.964-.432-.964-.964 0-.533.431-.964.964-.964.532 0 .963.431.963.964 0 .532-.431.964-.963.964zm-13.398-5.639c-.639 0-1.156-.518-1.156-1.156 0-.639.517-1.157 1.156-1.157.639 0 1.157.518 1.157 1.157 0 .638-.518 1.156-1.157 1.156zm5.783 0c-.639 0-1.156-.518-1.156-1.156 0-.639.517-1.157 1.156-1.157.639 0 1.157.518 1.157 1.157 0 .638-.518 1.156-1.157 1.156z"/>
+                                    <path d="M21.502 19.525c1.524-1.105 2.498-2.738 2.498-4.554 0-3.326-3.237-6.023-7.229-6.023s-7.229 2.697-7.229 6.023c0 3.327 3.237 6.024 7.229 6.024.825 0 1.621-.117 2.36-.33l.212-.032c.139 0 .265.043.384.111l1.583.914.139.045c.133 0 .241-.108.241-.241l-.039-.176-.326-1.215-.025-.154c0-.162.08-.305.202-.392zm-12.827-17.228c-4.791 0-8.675 3.236-8.675 7.229 0 2.178 1.168 4.139 2.997 5.464.147.104.243.276.243.471l-.03.184-.391 1.458-.047.211c0 .16.13.29.289.29l.168-.054 1.899-1.097c.142-.082.293-.133.46-.133l.255.038c.886.255 1.842.397 2.832.397l.476-.012c-.188-.564-.291-1.158-.291-1.771 0-3.641 3.542-6.593 7.911-6.593l.471.012c-.653-3.453-4.24-6.094-8.567-6.094zm5.686 11.711c-.532 0-.963-.432-.963-.964 0-.533.431-.964.963-.964.533 0 .964.431.964.964 0 .532-.431.964-.964.964zm4.82 0c-.533 0-.964-.432-.964-.964 0-.533.431-.964.964-.964.532 0 .963.431.963.964 0 .532-.431.964-.963.964zm-13.398-5.639c-.639 0-1.156-.518-1.156-1.156 0-.639.517-1.157 1.156-1.157.639 0 1.157.518 1.157 1.157 0 .638-.518 1.156-1.157 1.156zm5.783 0c-.639 0-1.156-.518-1.156-1.156 0-.639.517-1.157 1.156-1.157.639 0 1.157.518 1.157 1.157 0 .638-.518 1.156-1.157 1.156z" />
                                 </svg>
                             </a>
-                            <a href = "https://twitter.com/sperax_io" class="ml-3 text-gray-400">
+                            <a href="https://twitter.com/sperax_io" class="ml-3 text-gray-400">
                                 <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                                 </svg>
                             </a>
-                            <a href = "https://medium.com/sperax" class="ml-3 text-gray-400">
+                            <a href="https://medium.com/sperax" class="ml-3 text-gray-400">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                <path d="M2.846 6.887c.03-.295-.083-.586-.303-.784l-2.24-2.7v-.403h6.958l5.378 11.795 4.728-11.795h6.633v.403l-1.916 1.837c-.165.126-.247.333-.213.538v13.498c-.034.204.048.411.213.537l1.871 1.837v.403h-9.412v-.403l1.939-1.882c.19-.19.19-.246.19-.537v-10.91l-5.389 13.688h-.728l-6.275-13.688v9.174c-.052.385.076.774.347 1.052l2.521 3.058v.404h-7.148v-.404l2.521-3.058c.27-.279.39-.67.325-1.052v-10.608z"/>
+                                    <path d="M2.846 6.887c.03-.295-.083-.586-.303-.784l-2.24-2.7v-.403h6.958l5.378 11.795 4.728-11.795h6.633v.403l-1.916 1.837c-.165.126-.247.333-.213.538v13.498c-.034.204.048.411.213.537l1.871 1.837v.403h-9.412v-.403l1.939-1.882c.19-.19.19-.246.19-.537v-10.91l-5.389 13.688h-.728l-6.275-13.688v9.174c-.052.385.076.774.347 1.052l2.521 3.058v.404h-7.148v-.404l2.521-3.058c.27-.279.39-.67.325-1.052v-10.608z" />
                                 </svg>
                             </a>
-                            <a href = "https://www.linkedin.com/company/sperax/" class="ml-3 text-gray-400">
+                            <a href="https://www.linkedin.com/company/sperax/" class="ml-3 text-gray-400">
                                 <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
                                     <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                                     <circle cx="4" cy="4" r="2" stroke="none"></circle>
